5b6564880d436f627a93a40126489dffc75ced48
manav108-hub <manavadwani86@gmail.com>
2025-09-07 10:52:58 +0000

    addedd

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(
 );
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3001',
   credentials: true,
   exposedHeaders: ['set-cookie']
 }));
